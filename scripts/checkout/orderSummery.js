c5a40dbce520811930efa5753a5e23f33a22cbdf
BinMamun <binbean44@gmail.com>
2023-11-13 02:56:07 +0600

    create calculateDeliveryOption function & reuse it

--- scripts/checkout/orderSummery.js
+++ scripts/checkout/orderSummery.js
@@ -1,8 +1,7 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import formatCurrency from "../utils/money.js";
-import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js"
+import { calculateDeliveryDate, deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js"
 import { renderPaymentSummery } from "./paymentSummery.js";
 
 
@@ -17,10 +16,7 @@ export function renderOrderSummery() {
         const deliveryOptionId = cartItem.deliveryOptionId;
 
         const deliveryOption = getDeliveryOption(deliveryOptionId);
-
-        const today = dayjs();
-        const deliveryDate = today.add(deliveryOption.deliveryDays, "day");
-        const dateString = deliveryDate.format("dddd, MMMM D");
+        const dateString = calculateDeliveryDate(deliveryOption);
 
         cartSummeryHTML += `
 <div class="cart-item-container
@@ -176,12 +172,7 @@ export function renderOrderSummery() {
 
         let html = "";
         deliveryOptions.forEach((deliveryOption) => {
-            const today = dayjs();
-            const deliveryDate = today.add(
-                deliveryOption.deliveryDays,
-                "day"
-            );
-            const dateString = deliveryDate.format("dddd, MMMM D");
+            const dateString = calculateDeliveryDate(deliveryOption);
 
             const priceString = deliveryOption.priceCents === 0 ?
                 "FREE " : `${formatCurrency(deliveryOption.priceCents)} -`;
